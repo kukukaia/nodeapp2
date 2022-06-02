aabd77b9394cd90f39746f1c147e19cd6c4f8d14
kukukaia <kukukaia@protonmail.com>
2022-06-02 00:08:32 -0300

    alterando arquivos e criando arquivos ejs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -24,5 +24,26 @@ router.get('/contato',(req,res)=>{
     res.end("Pagina de contatos")
 })
 
+//outros métodos(aula de 01/06/22-quarta)
+router.post('/contato', (req, res)=>{
+    //res.end("Pagina de CONTATO")
+    let dados = JSON.stringify(req.body)
+    //res.end(dados)
+    res.end(dados)
+})
+
+router.patch('/contato', (req, res)=>{
+    res.end("Pagina de CONTATO")
+})
+
+router.post('/imc', (req, res)=>{
+    let imc=req.body.peso/(req.body.altura*req.body.altura)
+    //res.end("O IMC calculado é:  "+imc)
+    res.render('imc/resultado',{'resultado':imc})  
+})
+
+router.get('/imc', (req, res)=>{
+    res.render('imc/formulario')    
+})
 
 module.exports = router
--- routes/produtosRouter.js
+++ routes/produtosRouter.js
@@ -2,24 +2,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/camisetas', (req,res)=>{
-    res.end("Pagina de camisetas")
+    //res.end("Pagina de camisetas")
+    res.render('produtos/camisetas')
 })
 
 router.get('/calcas', (req,res)=>{
-    res.end("Pagina de calcas")
+    //res.end("Pagina de calcas")
+    res.render('produtos/calcas')
+})
+
+router.get('/sapatos', (req,res)=>{
+    //res.end("Pagina de sapatos")
+    res.render('produtos/sapatos')
+})
+
+router.get('/sapatos/:id2', (req,res)=>{
+    res.end(req.params.id2)
+    //res.end("Pagina de sapatos")
 })
 
 module.exports = router
-
-router.get('/sapatos', (req,res)=>{
-    res.end("Pagina de sapatos")
-})
-
-router.get('/sapatos/:id', (req,res)=>{
-    res.send(req.params.id)
-    res.end("Pagina de sapatos")
-})
-
-router.get('/bolsas', (req,res)=>{
-    res.end("Pagina de bolsas")
-})
